163a885fa3e865e4e55d948a6aa32a22eac6e37a
jdevenish <justin.devenish@gmail.com>
2020-04-20 16:24:02 -0400

    updating missed functions

--- routes/user.js
+++ routes/user.js
@@ -6,9 +6,5 @@ const withAuth = require('../middleware');
 // Update user's profile
 router.put('/update', userController.updateProfile);
 
-// Delete user's profile
-router.delete('/delete', userController.deleteProfile);
-
-
 
 module.exports = router;
